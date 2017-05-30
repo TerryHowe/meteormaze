d158c3fe6207a66eb9f7fc4a6f08503df1713354
Terry Howe <terrylhowe@gmail.com>
2017-05-30 08:44:31 -0600

    working goForward

--- imports/api/rooms.js
+++ imports/api/rooms.js
@@ -6,8 +6,12 @@ export const Rooms = new Meteor.Collection("rooms",
     {
         entry.goLeft = function(){ return(undefined);};
         entry.goRight = function(){ return(undefined);};
-        entry.goForward = function(){ return(undefined);};
-        entry.toString = function(){ return(this.x.toString() + "," + this.y.toString());};
+        entry.goForward = function(direction){
+          return(this.passages[direction]);
+        };
+        entry.toString = function(){
+          return(this.x.toString() + "," + this.y.toString());
+        };
         return entry;
     }
 });
