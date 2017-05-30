22419fda590346ad28bfe70771462f63c1d82097
Terry Howe <terrylhowe@gmail.com>
2017-05-30 08:48:25 -0600

    get right direction

--- imports/api/rooms.js
+++ imports/api/rooms.js
@@ -4,12 +4,26 @@ export const Rooms = new Meteor.Collection("rooms",
 {
     transform:function(entry)
     {
+        entry.getRightDirection = function(direction) {
+          switch (direction) {
+          case 'N':
+            return('E');
+          case 'E':
+            return('S');
+          case 'S':
+            return('W');
+          default:
+            return('N');
+          }
+        };
         entry.goLeft = function(){ return(undefined);};
-        entry.goRight = function(){ return(undefined);};
-        entry.goForward = function(direction){
+        entry.goRight = function(direction) {
+          return(this.passages[this.getRightDirection(direction)]);
+        };
+        entry.goForward = function(direction) {
           return(this.passages[direction]);
         };
-        entry.toString = function(){
+        entry.toString = function() {
           return(this.x.toString() + "," + this.y.toString());
         };
         return entry;
