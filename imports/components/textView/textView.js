07f60acfd920c850060f9c6fbcf9e6529b44c2cd
Terry Howe <terrylhowe@gmail.com>
2017-05-30 15:53:54 -0600

    direction changes

--- imports/components/textView/textView.js
+++ imports/components/textView/textView.js
@@ -240,8 +240,8 @@ class TextViewCtrl {
   constructor($scope) {
     $scope.viewModel(this);
     this.helpers({
-      room() {
-        return(this.render(this.get_room()));
+      view() {
+        return(this.render(this.direction));
       }
     })
   }
@@ -256,22 +256,27 @@ class TextViewCtrl {
     return(rooms[0]);
   }
 
-  render(room) {
+  render(direction) {
+    let room = this.get_room();
     if (typeof room == 'undefined') {
       return '';
     }
-    room = this.get_room();
+    if (direction == 'undefined') {
+      this.direction = 'E';
+    }
+    else {
+      this.direction = direction;
+    }
 
-    let direction = 'N';
     let t = [VIEW.split("")]
-    let left_room = room.goLeft(direction)
-    let forward_room = room.goForward(direction)
-    let right_room = room.goRight(direction)
+    let left_room = room.goLeft(this.direction)
+    let forward_room = room.goForward(this.direction)
+    let right_room = room.goRight(this.direction)
     if (typeof left_room === 'undefined') {
       t.push(LEFT.split(""))
     }
     else {
-      if (typeof left_room.goForward(direction) === 'undefined') {
+      if (typeof left_room.goForward(this.direction) === 'undefined') {
         t.push(LEFT_FORWARD_RIGHT.split(""))
       }
     }
@@ -279,24 +284,24 @@ class TextViewCtrl {
       t.push(FORWARD.split(""))
     }
     else {
-      let forward_left_room = forward_room.goLeft(direction)
+      let forward_left_room = forward_room.goLeft(this.direction)
       if (typeof forward_left_room === 'undefined') {
         t.push(FORWARD_LEFT.split(""))
       }
       else {
-        if (typeof forward_left_room.goForward(direction) === 'undefined') {
+        if (typeof forward_left_room.goForward(this.direction) === 'undefined') {
           t.push(FORWARD_LEFT_FORWARD_RIGHT.split(""))
         }
       }
-      if (typeof forward_room.goForward(direction) === 'undefined') {
+      if (typeof forward_room.goForward(this.direction) === 'undefined') {
         t.push(FORWARD_FORWARD.split(""))
       }
-      let forward_right_room = forward_room.goRight(direction)
+      let forward_right_room = forward_room.goRight(this.direction)
       if (typeof forward_right_room === 'undefined') {
         t.push(FORWARD_RIGHT.split(""))
       }
       else {
-        if (typeof forward_right_room.goForward(direction) === 'undefined') {
+        if (typeof forward_right_room.goForward(this.direction) === 'undefined') {
           t.push(FORWARD_RIGHT_FORWARD_LEFT.split(""))
         }
       }
@@ -305,7 +310,7 @@ class TextViewCtrl {
       t.push(RIGHT.split(""))
     }
     else {
-      if (typeof right_room.goForward(direction) === 'undefined') {
+      if (typeof right_room.goForward(this.direction) === 'undefined') {
         t.push(RIGHT_FORWARD_LEFT.split(""))
       }
     }
@@ -313,6 +318,24 @@ class TextViewCtrl {
     let z = zip(t);
     let result = z.map(row => {return row.reduce((a,b) => {return a>b?a:b;})});
     return(result.join(""));
+  }
+  addTask(newTask) {
+    this.newTask = '';
+    alert(this.direction);
+    switch (this.direction) {
+    case 'N':
+      this.direction = 'E';
+      break;
+    case 'E':
+      this.direction = 'S';
+      break;
+    case 'S':
+      this.direction = 'W';
+      break;
+    default:
+      this.direction = 'N';
+      break;
+    }
   }
 }
  
