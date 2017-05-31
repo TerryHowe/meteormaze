82ec50b31140b94d0e4879ba08725c409c8dac81
Terry Howe <terrylhowe@gmail.com>
2017-05-31 12:55:12 -0600

    some sort of global keypress that works

--- client/main.js
+++ client/main.js
@@ -2,7 +2,25 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import textView from '../imports/components/textView/textView';
  
-angular.module('text-maze', [
+var app = angular.module('text-maze', [
   angularMeteor,
   textView.name
+]);
+
+app.directive('keypressEvents', [
+  '$document',
+  '$rootScope',
+  function($document, $rootScope) {
+    return {
+      restrict: 'A',
+      link: function() {
+        $document.bind('keypress', function(e) {
+          alert('Got keypress:');
+          textView.controller.handleKeyDown(e);
+          $rootScope.$broadcast('keypress', e);
+          $rootScope.$broadcast('keypress:' + e.which, e);
+        });
+      }
+    };
+  }
 ]);
