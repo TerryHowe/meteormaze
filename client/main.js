6111a279f15fc877df0d4faed5add72b331e42d7
Terry Howe <terrylhowe@gmail.com>
2017-05-31 13:51:01 -0600

    remove directive

--- client/main.js
+++ client/main.js
@@ -2,25 +2,7 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import textView from '../imports/components/textView/textView';
  
-var app = angular.module('text-maze', [
+angular.module('text-maze', [
   angularMeteor,
   textView.name
-]);
-
-app.directive('keypressEvents', [
-  '$document',
-  '$rootScope',
-  function($document, $rootScope) {
-    return {
-      restrict: 'A',
-      link: function() {
-        $document.bind('keypress', function(e) {
-          alert('Got keypress:');
-          textView.controller.handleKeyDown(e);
-          $rootScope.$broadcast('keypress', e);
-          $rootScope.$broadcast('keypress:' + e.which, e);
-        });
-      }
-    };
-  }
 ]);
